a3592f63e95c3120d9a41c66f9b2a5a09ecd574a
Asep Wahyudi <asepwahyudi2@gmail.com>
2026-07-14 15:38:46 +0700

    Remove mock-cpus.js loader to bypass NODE_OPTIONS restriction on server

--- mock-cpus.js
+++
@@ -1,5 +0,0 @@
-const os = require('os');
-const originalCpus = os.cpus;
-os.cpus = () => {
-  return [{ model: 'mock-1-cpu', speed: 2000, times: { user: 0, nice: 0, sys: 0, idle: 0, irq: 0 } }];
-};
